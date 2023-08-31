661949446b8cb23b6bd484329575341e85ae2d8f
Luis Herrera <luis1818.lh@gmail.com>
2023-08-31 15:36:36 -0500

    Se crean los archivos para explicar el uso de variables, arrow function y strings con la actualización ECMAScrip 6

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,10 @@
+function square (num) {
+    return num*num;
+}
+
+
+const square = (num)=>{
+    return num * num;
+}
+
+const square = num => num * num; //esta arror function solo aplica cuando se tiene un solo parametro y el return se vuelve implicito.
---
+++ src/es6/03-strings.js
@@ -0,0 +1,16 @@
+let hello = "hello";
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase)
+
+//template literal
+let epicPhrase2 = `${hello} ${world} `;
+console.log(epicPhrase2);
+
+//Multi-line strings
+
+let lorem = 'esto es un string \n' + 'esto es otra linea';
+let lorem2 = `esta es una frase epica
+la continuación de la frase epica`;
+console.log(lorem);
+console.log(lorem2);
