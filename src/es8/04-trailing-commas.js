0779f8de57078faaad446369f89a8ab4fc422c1d
Luis Herrera <luis1818.lh@gmail.com>
2023-09-01 17:18:05 -0500

    Se agregan archivos con los cambios en ES8

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,3 @@
+const array = [24, 34, 25, 24, , , ,]; // se pueden colocar comas, las cuales generan mas espacios en la array, aunque no tengan valores pueden tomar el valor de empty
+console.log(array);
+console.log(array.length);
