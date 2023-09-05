23e78ca17a47f4f78d72d8ea404788d29970dd57
Luis Herrera <luis1818.lh@gmail.com>
2023-09-05 14:52:15 -0500

    Se agregan las clases relacionadas a ES12

---
+++ src/es12/03-private.js
@@ -0,0 +1,24 @@
+class user {
+    //constructor
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    //metodo
+    speak(){
+        return 'Hello';
+
+    }
+    greeting(){
+        return `${this.speak()} ${this.name}`;
+    }
+    get uAge(){
+        return this.age;
+    }
+    set uAge(n){
+        this.age = n;
+    }
+}
+const newContent = new user('David', 15);
+console.log(newContent.uAge);
+console.log(newContent.uAge = 20);
